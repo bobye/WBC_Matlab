247a09f86db118fbf6e0b346d1b0599f5d08375b
Jianbo Ye <yelpoo@gmail.com>
2014-11-24 00:35:43 -0500

    fix index error

--- profile_kantorovich.m
+++ profile_kantorovich.m
@@ -15,13 +15,13 @@ kantorovich_prepare(max_stride);
 % %% 
 tic;
   for s=1:s_modalities
-      idx=[1,cumsum(db{s}.stride)];
+      idx=[0,cumsum(db{s}.stride)]+1;
       for i=1:size
           for j=i+1:size
-              d1.supp = db{s}.supp(:,idx(i):idx(i+1)-1);
-              d1.w = db{s}.w(idx(i):idx(i+1)-1);
-              d2.supp = db{s}.supp(:,idx(j):idx(j+1)-1);
-              d2.w = db{s}.w(idx(j):idx(j+1)-1);
+              d1.supp = db{s}.supp(:,idx(i):(idx(i+1)-1));
+              d1.w = db{s}.w(idx(i):(idx(i+1)-1));
+              d2.supp = db{s}.supp(:,idx(j):(idx(j+1)-1));
+              d2.w = db{s}.w(idx(j):(idx(j+1)-1));
               [f0,x0,lambda0] = kantorovich(d1.supp, d1.w, d2.supp, d2.w);
               %[f1,x1,lambda1] = kantorovich_dual(d1.supp, d1.w, d2.supp, d2.w);
           end
