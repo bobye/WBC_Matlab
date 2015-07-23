fdf04b6e0a15fc159e260fec3e58bdadab35464c
Jianbo Ye <jxy198@ist.psu.edu>
2015-07-23 02:17:35 -0400

    update profile script

--- profile_centroid.m
+++ profile_centroid.m
@@ -2,11 +2,11 @@
 %% Load data
 clear;
 setparam;
-size=100;
+size=1000;
 
 s_modalities = 2;
 d_modalities = [3 3];
-filename='../mountaindat.txt';
+filename='../data/mountaindat.d2';
 db = loaddata(size, s_modalities, d_modalities, filename);
 
 max_stride = max(cellfun(@(x) max(x.stride), db));
@@ -21,21 +21,21 @@ kantorovich_prepare(max_stride);
 %profile off
 %profile viewer
 %%
-%profile on
-%for s=1:1 %1:s_modalities
-%    c.Bregman = centroid_sphBregman(db{s}.stride, db{s}.supp, db{s}.w, []);
-%    centroid_sphEnergy(db{s}.stride, db{s}.supp, db{s}.w, c.Bregman);    
-%end
-%profile off
-%profile viewer
-
-%%
 profile on
-for s=2:2 %1:s_modalities
-    c.ADMM = centroid_sphADMM(db{s}.stride, db{s}.supp, db{s}.w, []);
+for s=1:1 %1:s_modalities
+    c.Bregman = centroid_sphBregman(db{s}.stride, db{s}.supp, db{s}.w, []);
+    centroid_sphEnergy(db{s}.stride, db{s}.supp, db{s}.w, c.Bregman);    
 end
 profile off
 profile viewer
+
+%%
+%profile on
+%for s=2:2 %1:s_modalities
+%    c.ADMM = centroid_sphADMM(db{s}.stride, db{s}.supp, db{s}.w, []);
+%end
+%profile off
+%profile viewer
 %%
 %for s=1:1 %1:s_modalities
 %    c.LP = centroid_sphLP(db{s}.stride, db{s}.supp, db{s}.w);
